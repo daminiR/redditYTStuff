7c6e0f443e0afd9fed0fedeb32169f13d66d4b1a
Damini Rijhwani <rijhwanidamini@gmail.com>
2022-11-06 16:12:53 -0500

    changes "

--- amazonTextract/createVideo.py
+++ amazonTextract/createVideo.py
@@ -1,11 +1,12 @@
 from moviepy.editor import *
+import json
 
 def createVideo(redditFolder):
-    video = VideoFileClip("/home/damini/Downloads/Veil - 66423.mp4")
+    video = VideoFileClip("/Users/daminirijhwani/Downloads/Background - 13949.mp4")
+    # title = ImageClip(redditFolder + "/screenshots/Screenshot from 2022-11-04 22-06-06.png").set_start(3).set_duration(7).set_pos(("center","center"))
+    # final = CompositeVideoClip([video, title])
+    # final.write_videofile("./test.mp4")
+    with open(redditFolder + "/sync/screenshotTimestamps.json") as f:
+        timestamps = json.load(f)
+        comment = ImageClip(redditFolder + "/screenshots/Screenshot from 2022-11-04 22-06-06.png").set_start(3).set_duration(7).set_pos(("center","center"))
 
-    title = ImageClip("redditFolder" + "/screenshots/Screenshot from 2022-11-04 22-06-06.png").set_start(3).set_duration(7).set_pos(("center","center"))
-              #.resize(height=50) # if you need to resize...
-
-
-    final = CompositeVideoClip([video, title])
-    final.write_videofile("./test.mp4")
