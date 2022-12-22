3d19e10e34383eed2967daea2f4955eb5d83a440
Damini Rijhwani <rijhwanidamini@gmail.com>
2022-12-22 18:16:07 -0500

    all changes working

--- main/moviepy_processing.py
+++ main/moviepy_processing.py
@@ -6,11 +6,9 @@ from utils.edit_thumnail import createThumbnail
 if __name__ == "__main__":
     rootDir = sys.argv[1]
     videoType = sys.argv[2]
-    if len(sys.argv) == 3:
-        speed = sys.argv[3]
     if videoType == 'short':
-        print("create short audio")
-        createShorts(rootDir, speed)
+            print("create short audio")
+            createShorts(rootDir)
     elif videoType == 'long':
         print("create long audio")
         createVideo(rootDir)
--- step_5.sh
+++ step_5.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo "Enter the video type: "
 read videoType
+#if [ "$videoType" = "short" ]; then
+    #echo "Enter speed of short: "
+    #read shortSpeed
+#fi
+shortSpeed=1.5
 length_inputs=$(jq '.Inputs| length' inputs.json)
 rootFolderString=( $(jq .Inputs[].rootFolder inputs.json) )
 
@@ -8,5 +13,14 @@ for var_idx in  $(seq $length_inputs);
 do
     redditFolder=$(sed -e 's/^"//' -e 's/"$//' <<<"${rootFolderString[var_idx - 1]}")
     echo $redditFolder
+    if [ "$videoType" = "short" ]; then
+        echo "in shorts"
+        python3 ./main/moviepy_processing.py $redditFolder $videoType $shortSpeed
+        ffmpeg -i $redditFolder/youtubeVideo/yt_shorts_inter.mp4 \
+            -filter_complex "[0:v]setpts=0.667*PTS[v];[0:a]atempo=$shortSpeed[a]" \
+            -map "[v]" -map "[a]" $redditFolder/youtubeVideo/yt_shorts.mp4
+    else
+        python3 ./main/moviepy_processing.py $redditFolder $videoType
+    fi
     python3 ./main/moviepy_processing.py $redditFolder $videoType
 done
