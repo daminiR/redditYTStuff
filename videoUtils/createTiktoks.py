29923c79ce7df5554276423f85f21031d205e19f
Damini Rijhwani <rijhwanidamini@gmail.com>
2023-01-07 15:42:26 -0500

    too lost with the video part

--- videoUtils/createTiktoks.py
+++ videoUtils/createTiktoks.py
@@ -33,26 +33,28 @@ def createTiktoks(redditFolder):
         start_tiktok = 0
         all_comments = []
         offset = 0
+        title = None
         for timestamp in timestamps:
                 start = timestamp["Time"] / 1000
                 duration = timestamp["Duration"] / 1000
-                print("what time things start")
-                print("the actual start", start)
-                print(start - offset)
-                comment = ImageClip(redditFolder + "/screenshots_tiktok/" + timestamp["Filename"])\
-                .set_start(start - offset).set_duration(duration).set_pos(("center","center"))
+                if title == None:
+                    comment = ImageClip(redditFolder + "/screenshots_tiktok/" + timestamp["Filename"])\
+                    .set_start(start - offset).set_duration(duration).set_pos(("center","center"))
+                else:
+                    comment = ImageClip(redditFolder + "/screenshots_tiktok/" + timestamp["Filename"])\
+                    .set_start(start - offset + title[0].duration).set_duration(duration).set_pos(("center","center"))
                 if comment.size[1] > backgroundVideoSize[1]:
                     resizeComment = comment.resize((int(backgroundVideoSize[0] * 0.8), int(backgroundVideoSize[1])*0.95))
                 else:
                     resizeComment = comment.resize(width=int(backgroundVideoSize[0] * 0.9))
                 if timestamp["Mark Sentence"] == "TITLE":
+                    resizeComment.set_duration(duration)
                     title = [resizeComment, start, + duration]
                 else:
                     comments.append([resizeComment, start + duration])
                     all_comments.append([resizeComment, start + duration])
                 check_idx += 1
                 if start > length * length_idx:
-                    print("tiktok here")
                     length_idx += 1
                     end_tiktok = start + duration
                     total_comments = [title]
@@ -60,27 +62,38 @@ def createTiktoks(redditFolder):
                     tiktoks.append((total_comments, start_tiktok, end_tiktok))
                     start_tiktok = end_tiktok
                     comments = [title]
-                    print(end_tiktok)
-                    print("titlw in econds", title[0].duration)
-                    offset = offset + end_tiktok + (title[0].duration / 1000)
-                    print("in seconds?")
-                    print(offset)
-        only_comments = [x[0].set_pos("center", "center")  for x in all_comments]
-        final = CompositeVideoClip([*only_comments])
+                    offset =  end_tiktok
         for idx, tiktok in enumerate(tiktoks):
-            titleAudioClip = new_audioclip.subclip(title[1],  title[2] + 1)
-            tiktokAudioClip = new_audioclip.subclip(tiktok[1],  tiktok[2] + 1)
-            titleVideoClip = CompositeVideoClip([title[0]])
-            # tiktokVideoClip = final.subclip(tiktok[1], tiktok[2] + 1)
+            titleAudioClip = new_audioclip.subclip(title[1],  title[2])
+            tiktokAudioClip = new_audioclip.subclip(tiktok[1],  tiktok[2])
             tiktokVideoClip = tiktok[0]
             finalAudio = concatenate_audioclips([titleAudioClip, tiktokAudioClip])
-            # finalVideo = concatenate_videoclips([titleVideoClip, *tiktokVideoClip])
-            # total_background = max(finalVideo.duration, finalAudio.duration)
             loopedVideo = backgroundVideo.loop(duration=finalAudio.duration)
             tiktokVideoClip = [x[0].set_pos("center", "center")  for x in tiktokVideoClip]
+            tiktokVideoClip.append(title[0])
             final2 = CompositeVideoClip([loopedVideo, *tiktokVideoClip])
             final2.audio = finalAudio
             final2.write_videofile(redditFolder + "/youtubeVideo/" + "yt_tiktok_inter_" + str(idx + 1) + ".mp4", fps= 24)
+
+
+
+            # if idx == 5:
+                # break
+            # titleAudioClip = new_audioclip.subclip(title[1],  title[2] + 1)
+            # tiktokAudioClip = new_audioclip.subclip(tiktok[1],  tiktok[2] + 1)
+            # titleVideoClip = CompositeVideoClip([title[0]])
+            # # tiktokVideoClip = final.subclip(tiktok[1], tiktok[2] + 1)
+            # tiktokVideoClip = tiktok[0]
+            # finalAudio = concatenate_audioclips([titleAudioClip, tiktokAudioClip])
+            # # finalVideo = concatenate_videoclips([titleVideoClip, final2])
+            # # total_background = max(finalVideo.duration, finalAudio.duration)
+            # loopedVideo = backgroundVideo.loop(duration=finalAudio.duration)
+            # tiktokVideoClip = [x[0].set_pos("center", "center")  for x in tiktokVideoClip]
+            # final2 = CompositeVideoClip([titleVideoClip, *tiktokVideoClip])
+
+            # final2 = CompositeVideoClip([loopedVideo, final2])
+            # final2.audio = finalAudio
+            # final2.write_videofile(redditFolder + "/youtubeVideo/" + "yt_tiktok_inter_" + str(idx + 1) + ".mp4", fps= 24)
         # final.write_videofile(redditFolder + "/youtubeVideo/" + "yt_shorts.mp4", ffmpeg_params = ["-filter_complex", str(string1), "-map", "[v]", "-map", "[a]"])
 
         # while actual_length > desired_duration:
